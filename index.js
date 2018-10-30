4b81acc1c05b8fd0a47309f67ac65da2681bd996
siege918 <mebecjalt@gmail.com>
2018-10-29 20:00:22 -0700

    Implementation of GET command

---
+++ index.js
@@ -0,0 +1,30 @@
+var http = require('http');
+
+module.exports = {
+	get: get
+}
+
+function get(message, config)
+{
+	return new Promise(function(resolve) {
+		getPromise(message, config, resolve);
+	});
+}
+
+function getPromise(message, config, callback) {
+	var query = message.content.substring(message.content.indexOf(' ')).trim().replace(/ /g, "%20");
+	http.get(config.path,
+		function(response) {
+			var body = '';
+			response.on("data",
+				function(d) {
+					body += d;
+				}
+			);
+			response.on("end", function() {
+                var responseText = body.replace(/{response}/gm, body)
+                message.channel.send(responseText);
+                callback(responseText);
+			});
+		});
+}
